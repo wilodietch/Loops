70e3219e2c1ca76cbbc001b42fa963a229648dc5
wilodietch <wilodietch@gmail.com>
2020-09-19 00:24:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#Loops
+
+# for loops, foreach loops, while loops
+
+for i in range(5):
+    print("waffles", i)
+
+grocery = ["apples", "pears", "bannanas"]
+
+print(grocery)
+print(grocery[0])
+
+print()
+
+for i in grocery:
+    print(i)
